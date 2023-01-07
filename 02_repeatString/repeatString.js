caa962ae8ae0815a15fc773469bb9d952c6e00cc
Se-French <116462983+Se-French@users.noreply.github.com>
2023-01-07 11:33:36 -0500

    Add function that repeats a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+let string = 'hey';
+let num = 3;
 
-};
+const repeatString = function(string, num) {
+    let repeat = '';
+
+    if (num >=0){
+    while (num > 0){
+        repeat += string;
+        num--;
+    }
+    } else {
+        repeat = "ERROR";
+    }
+    return repeat;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
