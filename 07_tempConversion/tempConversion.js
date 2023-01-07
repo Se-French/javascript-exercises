431bdc5f722f07f82ae32d83321b86e780cb0e86
Se-French <116462983+Se-French@users.noreply.github.com>
2023-01-07 14:57:17 -0500

    Write functions to convert temperatures
    Write 2 functions to convert a given temperature from fahrenheit to celsius and vice versa.
    The final temperatures are to be rounded to one decimal place.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celsius = (temp - 32)*(5/9);
+  const celsRound = Math.round(celsius * 10)/10;
+  return celsRound;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheit = (temp * (9/5)) + 32;
+  const fahrRound = Math.round(fahrenheit * 10)/10;
+  return fahrRound;
+
 };
 
 // Do not edit below this line
