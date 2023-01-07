5098ed1dfc6dd55c7cc36dfe985981ef5d772383
Se-French <116462983+Se-French@users.noreply.github.com>
2023-01-07 11:36:12 -0500

    Add a function that removes arguments from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+let arr = [1, 2, 3, 4];
+let args = 3;
 
-};
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args){    
+        if (arr.includes(arg)){
+            let index = arr.indexOf(arg);
+            arr.splice(index, 1);  
+        } else {
+            continue;
+        }
+    }   
+    return arr;
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
