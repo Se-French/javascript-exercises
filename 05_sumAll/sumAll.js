40ca157ad3cc99d303746e96b9a8220dc4f3d28a
Se-French <116462983+Se-French@users.noreply.github.com>
2023-01-07 11:37:45 -0500

    Add a function that returns the sum
    Add a function that takes 2 integers and returns the sum of every number between and including them.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+let x = 1;
+let y = 4;
 
-};
+const sumAll = function(x, y) {
+
+
+    if ((x < 0 || y < 0) || (typeof x != 'number' || typeof y != 'number')) {
+        return 'ERROR';
+
+    } else if (x < y){
+        let sum = 0;
+        for (let num = x; num <= y; num++){
+            sum += num;
+        }
+
+        return sum;
+
+    } else if (x > y){
+        let sum = 0;
+        for (let num = x; num >= y; num--){
+            sum += num;
+        }
+
+        return sum;
+    }
+
+}    
 
 // Do not edit below this line
 module.exports = sumAll;
