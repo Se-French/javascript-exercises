edcff7ca4b8d128a5cbe154af37053126b1fb348
Se-French <116462983+Se-French@users.noreply.github.com>
2023-01-07 11:35:17 -0500

    Add a function that reverses it's input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const string = 'hello there';
 
+const reverseString = function(string) {
+    const arr = Array.from(string);
+    const reverse =[...arr].reverse();
+    let result = reverse.join("");
+    return result;
 };
 
 // Do not edit below this line
