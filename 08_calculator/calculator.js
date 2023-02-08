74f42c56d04451f0a935bea45c56dad1f79de047
Se-French <116462983+Se-French@users.noreply.github.com>
2023-02-07 19:42:53 -0500

    Complete functions to pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x, y) {
+	let sum = x + y;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	let sub = x - y;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(array) {  
+  let total = array.reduce((a,b) => {
+    return a + b;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = array.reduce((a,b) => {
+    return a * b;
+  }, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) { 
+  let total = Math.pow(x, y);
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (let i = num-1; i >= 1; i--){
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
