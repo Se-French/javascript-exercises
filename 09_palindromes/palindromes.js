5da52caa161938f569344d835a72cec65d455944
Se-French <116462983+Se-French@users.noreply.github.com>
2023-02-08 20:27:12 -0500

    Create function to test for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    let newString = string.replace(/ /g, '').replace(/[^a-zA-Z]/g, '');
+    const arr = Array.from(newString);
+    let result = [...arr].reverse().join('');
+    if (newString.toLowerCase() === result.toLowerCase()){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
