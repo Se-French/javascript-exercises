002b4e16335c7e6d9705f18be92cb96b3c21fa03
Se-French <116462983+Se-French@users.noreply.github.com>
2023-02-09 18:55:12 -0500

    Create a function to pass all fibonacci tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num;
+    if (n < 0){
+        return 'OOPS'
+    } else if (typeof(n) === 'string'){
+        num = (+n) - 1;
+    } else {
+         num = n - 1;
+    }
+    let x = 0;
+    let y = 1;
+    let fib;
+    let i = 0;
+    for ( i = 0; i < num; i++){
+        fib = x + y;
+        x = y;
+        y = fib;
+    }
+    return y;
 };
 
 // Do not edit below this line
