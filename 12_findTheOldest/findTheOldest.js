d05b4e6e1796862f19cbc116fe71e4e9bf093622
Se-French <116462983+Se-French@users.noreply.github.com>
2023-02-10 14:57:41 -0500

    Create a function to find the correct object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if ('yearOfDeath' in person === true){
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person['age'] = new Date().getFullYear() - person.yearOfBirth;
+        }
+    });
+    people.sort((a,b) => (a.age > b.age) ? -1 : 1);
+    return people[0];
 };
 
 // Do not edit below this line
